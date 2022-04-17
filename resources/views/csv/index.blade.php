b5bcf6a9d036a1f56c9b66a055bf032e3d239b71
fumn112026 <fumn112026@gmail.com>
2022-04-17 11:04:04 +0900

    テーブルを作成
    ひとまず決め打ちでデータを記述

--- resources/views/csv/index.blade.php
+++ resources/views/csv/index.blade.php
@@ -7,6 +7,21 @@
   <title>Document</title>
 </head>
 <body>
-  test
+  <table>
+    <thead>
+      <tr><th>ID</th><th>性</th><th>名</th></tr>
+    </thead>
+    <tbody>
+      <tr><td>1</td><td>田中</td><td>太郎</td></tr>
+      <tr><td>2</td><td>山田</td><td>花子</td></tr>
+      <tr><td>3</td><td>佐藤</td><td>次郎</td></tr>
+      <tr><td>4</td><td>渡辺</td><td>三郎</td></tr>
+      <tr><td>5</td><td>佐々木</td><td>幸子</td></tr>
+    </tbody>
+  </table>
+
+  <form action="/" method="post">
+    <input type="submit" value="CSV形式でダウンロード">
+  </form>
 </body>
 </html>
