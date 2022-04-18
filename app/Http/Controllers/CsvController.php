1043042bec16ac37117de60aee1e0e26b959557e
fumn112026 <fumn112026@gmail.com>
2022-04-18 23:14:47 +0900

    ダウンロード機能のフォーマットを作成

--- app/Http/Controllers/CsvController.php
+++ app/Http/Controllers/CsvController.php
@@ -9,4 +9,8 @@ class CsvController extends Controller
     public function index(Request $request) {
         return view('csv.index');
     }
+
+    public function download() {
+        return view('csv.index');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\CsvController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [CsvController::class, 'index']);
+Route::post('/', [CsvController::class, 'download']);
