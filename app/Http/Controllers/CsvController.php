554c10f12dac1de04f3f99d86504aaeb379372ed
fumn112026 <fumn112026@gmail.com>
2022-04-19 23:01:11 +0900

    downloadメソッドを修正

--- app/Http/Controllers/CsvController.php
+++ app/Http/Controllers/CsvController.php
@@ -10,7 +10,16 @@ class CsvController extends Controller
         return view('csv.index');
     }
 
-    public function download() {
-        return view('csv.index');
+    public function download(Request $request) {
+        return response()->streamDownload(
+            function() {
+                // 出力バッファ
+                // 文字コードの変換
+                // ヘッダー
+                // データ
+
+
+            }
+        );
     }
 }
